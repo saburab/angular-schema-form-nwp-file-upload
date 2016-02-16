139ecd7966ca209712cde04770ae12fd0a1e217a
saburab <abu-rab@netzwerkplan.de>
2016-02-16 17:10:33 +0100

    * make example html file running again * get rid of obsolete schema.json file (now schema directly inside of app.js) * redefine js and css sources

--- app.js
+++ app.js
@@ -2,19 +2,79 @@
 
 var myApp = angular.module('formApp', [
    'schemaForm',
+   'pascalprecht.translate',
    'ngSchemaFormFile'
-]);
+])
+.controller('formController', ['$scope', '$q', function ($scope, $q) {
+  
+  $scope.schema = {
+    "type": "object",
+    "title": "Album",
+    "properties": {
+      "image": {
+        "title": "Image",
+        "type": "array",
+        "format": "singlefile",
+        "x-schema-form": {
+          "type": "array"
+        },
+        "pattern": {
+          "mimeType": "image/*",
+          "validationMessage": "Falscher Dateityp: "
+        },
+        "maxSize": {
+          "maximum": "2MB",
+          "validationMessage": "Erlaubte Dateigröße überschritten: ",
+          "validationMessage2": "Aktuelle Dateigröße: "
+        },
+        "maxItems": {
+          "validationMessage": "Es wurden mehr Dateien hochgeladen als erlaubt."
+        },
+        "minItems": {
+          "validationMessage": "Sie müssen mindestens eine Datei hochladen"
+        }
+      },
+      "images": {
+        "title": "Images",
+        "type": "array",
+        "format": "multifile",
+        "x-schema-form": {
+          "type": "array"
+        },
+        "pattern": {
+          "mimeType": "image/*,!.gif",
+          "validationMessage": "Falscher Dateityp: "
+        },
+        "maxSize": {
+          "maximum": "2MB",
+          "validationMessage": "Erlaubte Dateigröße überschritten: ",
+          "validationMessage2": "Aktuelle Dateigröße: "
+        },
+        "maxItems": {
+          "validationMessage": "Es wurden mehr Dateien hochgeladen als erlaubt."
+        },
+        "minItems": {
+          "validationMessage": "Sie müssen mindestens eine Datei hochladen"
+        }
+      }
+    },
+    "required": [
+      "images"
+    ]
+  };
 
-// our controller for the form
-// =============================================================================
-myApp.controller('formController', ['$scope', '$http', function ($scope, $http) {
-
-   $http.get('schema.json').success(function (data) {
-      $scope.schema = data.schema;
-      $scope.form   = data.form;
-   });
-
-   $scope.model = {};
+  $scope.form = [
+  {
+    "key": "image",
+    "type": "nwpFileUpload",
+    "endpoint": "https://angular-file-upload-cors-srv.appspot.com/upload"
+  }, {
+    "key": "images",
+    "type": "nwpFileUpload",
+    "endpoint": "https://angular-file-upload-cors-srv.appspot.com/upload"
+  }];
+  
+  $scope.model = {};
 
    $scope.submit = function () {
       $scope.$broadcast('schemaFormValidate');
@@ -23,5 +83,22 @@ myApp.controller('formController', ['$scope', '$http', function ($scope, $http) 
       }
    };
 
+}])
+.config(['$translateProvider', function($translateProvider) {
+  // Simply register translation table as object hash
+  $translateProvider.translations('en', {
+    'modules.upload.dndNotSupported': 'Drag n drop not surpported by your browser',
+    'modules.attribute.fields.required.caption': 'Required',
+    'modules.upload.descriptionMultifile': 'Drop your file(s) here',
+    'buttons.add': 'Open file browser',
+    'modules.upload.field.filename': 'Filename',
+    'modules.upload.field.preview': 'Preview',
+    'modules.upload.multiFileUpload': 'Multifile upload',
+    'modules.upload.field.progress': 'Progress',
+    'buttons.upload': 'Upload'
+  });
+  $translateProvider.preferredLanguage('en');
+
 }]);
+
 
